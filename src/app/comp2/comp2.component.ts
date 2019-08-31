9a82db46ff9822b6953d404e25ebea93afda64c1
Christian Liebel <christian.liebel@thinktecture.com>
2019-08-31 09:37:28 +0200

    demo-5

--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comp2',
   templateUrl: './comp2.component.html',
   styleUrls: ['./comp2.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Comp2Component implements OnInit {
   private myText = '';
